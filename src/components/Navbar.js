a6baedfb9f8490234d954271799d17d65ce7d7a8
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-10-26 11:06:29 +0330

    created banner top

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="h-13 md:h-[70px] bg-primary flex items-center justify-center">
+        <p className="text-sm md:text-base text-white font-semibold">تا ۷۰٪ تخفیف برای لوازم جانبی اورجینال آیفون</p>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Navbar;
