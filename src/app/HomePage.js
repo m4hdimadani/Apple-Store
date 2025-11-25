0dd15c16d6082a4d1281f074afd103243813517e
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-11-25 14:58:17 +0330

    layout from footer project

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -7,6 +7,7 @@ import BannerTow from "@/components/BannerTow";
 import BestProductsSwiper from "@/components/BestProductsSwiper";
 import Category from "@/components/Category";
 import CountDownPage from "@/components/CountDownPage";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import InformationBanner from "@/components/InformationBanner";
 import WatchList from "@/components/WatchList";
@@ -28,6 +29,7 @@ function HomePage() {
       <WatchList />
       <BannerTow />
       <AdvantagesOfTheWebsite />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+
+function Footer() {
+  return (
+    <div className="bg-lightGray">
+        <div className="container m-auto px-6 grid grid-cols-1  lg:grid-cols-4 gap-6">
+            <div className="w-full h-20 lg:order-3 lg:col-span-2"></div>
+            <div className="w-full h-20 lg:order-1 lg:col-span-4"></div>
+            <div className="w-full h-20 lg:order-4"></div>
+            <div className="w-full h-20 lg:order-2"></div>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
