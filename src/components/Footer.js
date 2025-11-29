95b08f9d7aef3e3cede8b4a305f32dc5af465e38
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-11-29 14:55:14 +0330

    created enama and socital medeia

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,10 @@
 
+import Image from "next/image"
+import enamad from "../assets/images/enamad.webp"
+import instagram from "../assets/svg/instagram.svg"
+import whatsapp from "../assets/svg/whatsapp.svg"
+import x from "../assets/svg/x.svg"
+
 function Footer() {
   return (
     <div className="bg-lightGray py-4">
@@ -7,16 +13,31 @@ function Footer() {
               <p className="font-medium">عضویت در خبرنامه</p>
               <div className="bg-white flex justify-between p-2 mt-2 rounded-[7px]">
                 <input type="text" className="flex-1 placeholder:text-[#2222224a] px-2" placeholder="ایمیل خود را وارد کنید"/>
-                <button className="bg-[#ff510c21] text-primary py-1 px-3 rounded-[7px]">
+                <button className="bg-[#ff510c21] text-primary py-1 px-3 rounded-[7px] cursor-pointer hover:bg-[#ff510c7a]">
                   عضویت
                 </button>
               </div>
             </div>
             <div className="w-full  lg:order-1 lg:col-span-4"></div>
-            <div className="w-full  lg:order-4"></div>
-            <div className="w-full  lg:order-2"></div>
+            <div className="w-full  lg:order-4 flex justify-between lg:justify-end gap-2 items-center lg:items-end max-lg:bg-white rounded-2xl max-lg:py-4 max-lg:px-5">
+              <div className="lg:bg-white lg:h-12 lg:w-12 lg:rounded-lg flex items-center justify-center cursor-pointer">
+                <Image src={instagram} alt="instagram" className="w-5 lg:w-6"/>
+              </div>
+              <div className="lg:bg-white lg:h-12 lg:w-12 lg:rounded-lg flex items-center justify-center cursor-pointer">
+                <Image src={whatsapp} alt="whatsapp" className="w-5 lg:w-6"/>
+              </div>
+              <div className="lg:bg-white lg:h-12 lg:w-12 lg:rounded-lg flex items-center justify-center cursor-pointer">
+                <Image src={x} alt="x" className="w-5 lg:w-6"/>
+              </div>
+             
+            </div>
+            <div className="w-full  lg:order-2">
+              <Image src={enamad} alt="enamad" className="w-15 lg:w-52"/>
+            </div>
         </div>
-        <div></div>
+        <div>
+          <p></p>
+        </div>
     </div>
   )
 }
